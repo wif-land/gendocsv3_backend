fb514c0251db8a19f6020b763779c9387f07c7e6
leninner <mazabandalenin180@gmail.com>
2023-12-26 23:27:29 -0500

    Update versions

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './users.entity'
-import { UsersController } from './users.controller';
+import { UsersController } from './users.controller'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
