d4e7ded7175fd1fee3c74bde5b6e1d52ce076d5b
leninner <mazabandalenin180@gmail.com>
2023-12-26 19:20:22 -0500

    Update project with auth module

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -21,7 +21,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('secret'),
+      secretOrKey: configService.get<string>('jwt.secret'),
     })
   }
 
