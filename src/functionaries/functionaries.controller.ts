af6f1abc880fa9aae0f78176882bb00ec97150bc
SrPabvliss <pablomartinvillacres@gmail.com>
2024-02-14 23:13:54 -0500

    bulkUpdate and findByField endpoints created, pagination implemented

--- src/functionaries/functionaries.controller.ts
+++ src/functionaries/functionaries.controller.ts
@@ -7,16 +7,29 @@ import {
   Delete,
   ParseIntPipe,
   Patch,
+  Query,
 } from '@nestjs/common'
 import { FunctionariesService } from './functionaries.service'
 import { CreateFunctionaryDto } from './dto/create-functionary.dto'
 import { ApiResponse, ApiTags } from '@nestjs/swagger'
 import { FunctionaryEntity } from './entities/functionary.entity'
+import { PaginationDto } from '../shared/dtos/pagination.dto'
+import { UpdateFunctionaryDto } from './dto/update-functionary.dto'
+import { UpdateFunctionariesBulkItemDto } from './dto/update-functionaries-bulk.dto'
 
 @ApiTags('Functionaries')
 @Controller('functionaries')
 export class FunctionariesController {
   constructor(private readonly functionariesService: FunctionariesService) {}
+
+  @Patch(`bulk`)
+  async bulkUpdate(
+    @Body() updateFunctionariesBulkDto: UpdateFunctionariesBulkItemDto[],
+  ) {
+    return await this.functionariesService.bulkUpdate(
+      updateFunctionariesBulkDto,
+    )
+  }
 
   @Post()
   async create(@Body() createFunctionaryDto: CreateFunctionaryDto) {
@@ -25,20 +38,23 @@ export class FunctionariesController {
 
   @ApiResponse({ isArray: true, type: FunctionaryEntity })
   @Get()
-  async findAll() {
-    return await this.functionariesService.findAll()
+  async findAll(@Query() paginationDto: PaginationDto) {
+    return await this.functionariesService.findAll(paginationDto)
   }
 
-  @ApiResponse({ type: FunctionaryEntity })
-  @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number) {
-    return await this.functionariesService.findOne(id)
+  @ApiResponse({ isArray: true, type: FunctionaryEntity })
+  @Get(`:field`)
+  async findByField(
+    @Param('field') field: string,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    return await this.functionariesService.findByField(field, paginationDto)
   }
 
   @Patch(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
-    @Body() updateFunctionaryDto: Partial<CreateFunctionaryDto>,
+    @Body() updateFunctionaryDto: UpdateFunctionaryDto,
   ) {
     return await this.functionariesService.update(id, updateFunctionaryDto)
   }
--- src/functionaries/functionaries.service.ts
+++ src/functionaries/functionaries.service.ts
@@ -9,6 +9,9 @@ import { CreateFunctionaryDto } from './dto/create-functionary.dto'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { FunctionaryEntity } from './entities/functionary.entity'
+import { PaginationDto } from '../shared/dtos/pagination.dto'
+import { UpdateFunctionaryDto } from './dto/update-functionary.dto'
+import { UpdateFunctionariesBulkItemDto } from './dto/update-functionaries-bulk.dto'
 
 @Injectable()
 export class FunctionariesService {
@@ -34,19 +37,29 @@ export class FunctionariesService {
     }
   }
 
-  async findAll(): Promise<FunctionaryEntity[]> {
+  async findAll(paginationDto: PaginationDto) {
+    // eslint-disable-next-line no-magic-numbers
+    const { limit = 5, offset = 0 } = paginationDto
+
     try {
       const functionaries = await this.functionaryRepository.find({
         order: {
           id: 'ASC',
         },
+        take: limit,
+        skip: offset,
       })
 
       if (!functionaries) {
         throw new NotFoundException('Functionaries not found')
       }
 
-      return functionaries
+      const count = await this.functionaryRepository.count()
+
+      return {
+        count,
+        functionaries,
+      }
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
     }
@@ -66,9 +79,42 @@ export class FunctionariesService {
     }
   }
 
+  async findByField(field: string, paginationDto: PaginationDto) {
+    // eslint-disable-next-line no-magic-numbers
+    const { limit = 5, offset = 0 } = paginationDto
+
+    try {
+      const queryBuilder =
+        this.functionaryRepository.createQueryBuilder('functionaries')
+
+      const functionaries = await queryBuilder
+        .where(
+          'UPPER(functionaries.first_name) like :field or UPPER(functionaries.second_name) like :field or UPPER(functionaries.first_last_name) like :field or UPPER(functionaries.second_last_name) like :field or functionaries.dni like :field',
+          { field: `%${field.toUpperCase()}%` },
+        )
+        .orderBy('functionaries.id', 'ASC')
+        .take(limit)
+        .skip(offset)
+        .getMany()
+
+      if (!functionaries) {
+        throw new NotFoundException('Functionaries not found')
+      }
+
+      const count = await queryBuilder.getCount()
+
+      return {
+        count,
+        functionaries,
+      }
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
   async update(
     id: number,
-    updateFunctionaryDto: Partial<CreateFunctionaryDto>,
+    updateFunctionaryDto: UpdateFunctionaryDto,
   ): Promise<FunctionaryEntity> {
     try {
       const functionary = await this.functionaryRepository.preload({
@@ -82,6 +128,42 @@ export class FunctionariesService {
 
       return await this.functionaryRepository.save(functionary)
     } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+  async bulkUpdate(
+    updateFunctionariesBulkDto: UpdateFunctionariesBulkItemDto[],
+  ) {
+    const queryRunner =
+      this.functionaryRepository.manager.connection.createQueryRunner()
+
+    await queryRunner.connect()
+
+    try {
+      await queryRunner.startTransaction()
+
+      const updatedCouncils = []
+
+      for (const updateFunctionary of updateFunctionariesBulkDto) {
+        const functionary = await this.functionaryRepository.preload({
+          id: updateFunctionary.id,
+          ...updateFunctionary,
+        })
+
+        if (!functionary) {
+          throw new NotFoundException('Functionary not found')
+        }
+
+        updatedCouncils.push(await queryRunner.manager.save(functionary))
+      }
+
+      await queryRunner.commitTransaction()
+
+      return updatedCouncils
+    } catch (error) {
+      await queryRunner.rollbackTransaction()
+
       throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
     }
   }
