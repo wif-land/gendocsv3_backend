b76cccb23b103021587e28c8e5f639472b25196d
devjaes <merajair17@gmail.com>
2024-05-30 14:00:27 -0500

    Add council to merge response

--- src/documents/services/document-recopilation.service.ts
+++ src/documents/services/document-recopilation.service.ts
@@ -108,6 +108,7 @@ export class DocumentRecopilationService {
     return new ApiResponseDto('Recopilación de documentos creada', {
       documentsRecopilated: resolvedDocuments.length,
       mergedDocument: !!mergedDocument.data.mergedDocumentPath,
+      council: mergedDocument.data.council,
     })
   }
 
@@ -204,8 +205,17 @@ export class DocumentRecopilationService {
       )
     }
 
+    const councilWithRecopilation = await this.dataSource.manager
+      .getRepository(CouncilEntity)
+      .findOne({
+        where: {
+          id: councilId,
+        },
+      })
+
     return new ApiResponseDto('Documentos del consejo procesados', {
       mergedDocumentPath,
+      council: councilWithRecopilation,
     })
   }
 
