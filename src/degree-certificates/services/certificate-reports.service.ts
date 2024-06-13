c19cc5ec4c8a958211928befae1ec684c7749ba3
devjaes <merajair17@gmail.com>
2024-06-13 17:38:23 -0500

    Separate numeration service

--- src/degree-certificates/services/certificate-reports.service.ts
+++ src/degree-certificates/services/certificate-reports.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
+import { DEGREE_CERTIFICATE } from '../constants'
+import { DegreeCertificateRepository } from '../repositories/degree-certificate-repository'
 
 @Injectable()
-export class CertificateReportsService {}
+export class CertificateReportsService {
+  constructor(
+    @Inject(DEGREE_CERTIFICATE.REPOSITORY)
+    private readonly degreeCertificateRepository: DegreeCertificateRepository,
+  ) {}
+
+  generate
+}
