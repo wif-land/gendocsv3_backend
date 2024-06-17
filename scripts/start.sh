a59c61be282a6bd2f7e6e8994e919604df4600b1
Lenin Mazabanda <mazabandalenin180@gmail.com>
2024-06-17 07:56:43 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 # Inicia la aplicación en segundo plano
 npm run start:prod &
 
-# Espera a que la aplicación esté lista (modificar este comando según sea necesario para tu aplicación)
+# Espera a que la aplicación esté lista
 while ! nc -z localhost 3001; do
   sleep 1
 done
