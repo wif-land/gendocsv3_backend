d65a9d2c4ce0b73de43bf8f88e25a0c32dc6c812
leninner <mazabandalenin180@gmail.com>
2024-06-18 19:15:08 -0500

    Update jwt auth guard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,14 @@
-import { HttpException, Injectable } from '@nestjs/common'
+import { HttpException, Injectable, Logger } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { HttpCodes } from '../../shared/enums/http-codes'
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  private readonly logger = new Logger(JwtAuthGuard.name)
   handleRequest(err, user) {
     if (err || !user) {
+      this.logger.error('Error de autenticación', err, user)
+
       throw new HttpException(
         'Error de autenticación. Por favor, inicie sesión.',
         HttpCodes.UNAUTHORIZED,
