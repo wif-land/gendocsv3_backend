43468f4343f3707fb60da405445d51adf22416ba
devjaes <merajair17@gmail.com>
2024-08-18 21:41:17 -0500

    chore: set syncronize true before migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -53,7 +53,7 @@ const config = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+  synchronize: true,
   entities: [`${__dirname}/**/*.entity{.ts,.js}`],
   keepConnectionAlive: true,
   migrationsRun: false,
